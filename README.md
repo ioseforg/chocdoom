fd197a8292bb4edebc9a4f6dc3776867b4ca7705
Joey Mann <mann@iosef.org>
2018-07-09 23:34:09 +1000

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# chocdoom - Chocolate Doom for UWP
+
+Chocolate Doom is a source port of the Doom engine which is capable of running DOOM, Heretic, Hexen and Strife. This port to the Xbox One is intended only to run these games in singleplayer mode with the Xbox One Controller or a Keyboard and Mouse (when such support arrives). The aim of this port is to maintain as much vanilla DOOM functionality as possible but make allowances for ease-of-use on console. 
+
+## Dependencies
+To use this repo, you'll need SDL2, SDL2_mix, and SDL2_net. These are not included.
---
+++ chocdoom/config.h
@@ -0,0 +1,27 @@
+// Dummy config.h for developing launcher.  Eventually, the launcher
+// will include the top-level config.h for the program.
+
+/* Name of package */
+#define PACKAGE "chocolate-doom"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "mann@iosef.org"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "Chocolate Doom"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "Chocolate Doom (UWP) 3.0.0"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "chocolate-uwp-doom"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "3.0.0"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Version number of package */
+#define VERSION "3.0.0"
+#pragma once
